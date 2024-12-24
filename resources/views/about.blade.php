b0e173d45fa3278efb1ba852c0f790da1f9464f4
Abdullah TATLICAK <tatlicak@gmail.com>
2024-12-24 07:18:09 +0300

    First Route

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+<h1>About Page</h1>
+<p> Sunt labore ipsum laboris id laborum aute elit irure elit adipisicing sit reprehenderit laborum duis. 
+    Dolore aute mollit voluptate deserunt pariatur proident. 
+    Laboris laborum amet pariatur laboris laboris et exercitation adipisicing cillum labore. 
+    Officia esse incididunt id excepteur amet officia. 
+    Ex proident commodo est non reprehenderit adipisicing ullamco tempor ex. 
+    Dolor laboris Lorem ea deserunt nulla id.</p>
