82cdcb92f4d77dce98ea32bbca652f43843c3e8a
Abdullah TATLICAK <tatlicak@gmail.com>
2024-12-25 10:22:30 +0300

    Create HomeController for Project

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ShowCarController;
 use App\Http\Controllers\MathOperationController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController ::class, 'index'])->name('home'); 
 
 Route::get('/about', function () {
     return view('about');
