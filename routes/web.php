b89143e8e7483316d029627c815550e7f1affc1b
Abdullah TATLICAK <tatlicak@gmail.com>
2024-12-27 08:22:51 +0300

    Create Signup page and extend it from clean layout

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ShowCarController;
 use App\Http\Controllers\MathOperationController;
+use App\Http\Controllers\SignupController;
 
 Route::get('/', [HomeController ::class, 'index'])->name('home'); 
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/signup', [SignupController::class, 'create'])->name('signup.create');
