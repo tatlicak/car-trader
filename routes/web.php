24c840e06e8538a66b0ab2759af4ecdb7cd0d9b5
Abdullah TATLICAK <tatlicak@gmail.com>
2024-12-31 06:30:41 +0300

    Cleanup Code From Views

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CarController;
-use App\Http\Controllers\HelloController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ShowCarController;
-use App\Http\Controllers\MathOperationController;
 use App\Http\Controllers\SignupController;
 
 Route::get('/', [HomeController ::class, 'index'])->name('home'); 
 
-Route::get('/signup', [SignupController::class, 'create'])->name('signup.create');
+Route::get('/signup', [SignupController::class, 'create'])->name('signup');
 
-Route::get('/login',[LoginController::class,'show'])->name('login.show');
+Route::get('/login',[LoginController::class,'show'])->name('login');
