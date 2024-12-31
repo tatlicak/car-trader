7f33169f616ccf968416a98d4f7ac5a6ba0000f0
Abdullah TATLICAK <tatlicak@gmail.com>
2024-12-31 07:02:42 +0300

    Create CarController and its Views

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SignupController;
 
 Route::get('/', [HomeController ::class, 'index'])->name('home'); 
 
-Route::get('/signup', [SignupController::class, 'create'])->name('signup');
+Route::get('/car/search', [CarController::class, 'search'])->name('car.search');
+Route::resource('car', CarController::class);
 
+Route::get('/signup', [SignupController::class, 'create'])->name('signup');
 Route::get('/login',[LoginController::class,'show'])->name('login');
